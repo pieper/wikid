12cfd60e020b6df1bb02cc77d1f8b982265b6358
Steve Pieper <pieper@bwh.harvard.edu>
2010-08-28 16:51:07 -0400

    strip out most of the demo content and add search and results elements

--- wikid.js
+++ wikid.js
@@ -6,28 +6,33 @@ var jQT = $.jQTouch({
 });
 
 $(document).ready(function() {
-    $('#createEntry form').submit(createEntry);
+    $('#home form').submit(search);
     $('#settings form').submit(saveSettings);
     $('#settings').bind('pageAnimationStart', loadSettings);
-    $('#dates li a').click(function(){
-        var dayOffset = this.id;
-        var date = new Date();
-        date.setDate(date.getDate() - dayOffset);
-        sessionStorage.currentDate = date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear();
-        refreshEntries();
-    });
+
     var shortName = 'Wikid';
-    var version = '1.0';
+    var version = '0.1';
     var displayName = 'Wikid';
     var maxSize = 65536;
-    db = openDatabase(shortName, version, displayName, maxSize);
+    db = openDatabase('wikid', version, displayName, maxSize);
+
     db.transaction(
         function(transaction) {
             transaction.executeSql(
-                'CREATE TABLE IF NOT EXISTS entries ' +
+                'CREATE TABLE IF NOT EXISTS dictionary ' +
                 ' (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
-                ' date DATE NOT NULL, food TEXT NOT NULL, ' +
-                ' calories INTEGER NOT NULL );'
+                ' english DATE NOT NULL, ' +
+                ' spanish INTEGER NOT NULL );'
+            );
+        }
+    );
+    db.transaction(
+        function(transaction) {
+            transaction.executeSql(
+                'INSERT INTO dictionary (english, spanish) VALUES (?, ?);',
+                ['h', 'h'],
+                function(){},
+                errorHandler
             );
         }
     );
@@ -47,50 +52,26 @@ function loadSettings() {
     $('#weight').val(localStorage.weight);
 }
 
-function refreshEntries() {
-    var currentDate = sessionStorage.currentDate;
-    $('#date h1').text(currentDate);
-    $('#date ul li:gt(0)').remove();
+function search() {
+    var searchString = $('#searchString').val();
     db.transaction(
         function(transaction) {
             transaction.executeSql(
-                'SELECT * FROM entries WHERE date = ? ORDER BY food;',
-                [currentDate],
+                'SELECT * FROM dictionary WHERE english = ? ORDER BY english;',
+                [searchString],
                 function (transaction, result) {
                     for (var i=0; i < result.rows.length; i++) {
                         var row = result.rows.item(i);
-                        var newEntryRow = $('#entryTemplate').clone();
+                        var newEntryRow = $('#resultTemplate').clone();
                         newEntryRow.removeAttr('id');
                         newEntryRow.removeAttr('style');
                         newEntryRow.data('entryId', row.id);
-                        newEntryRow.appendTo('#date ul');
-                        newEntryRow.find('.label').text(row.food);
-                        newEntryRow.find('.calories').text(row.calories);
-                        newEntryRow.find('.delete').click(function(){
-                            var clickedEntry = $(this).parent();
-                            var clickedEntryId = clickedEntry.data('entryId');
-                            deleteEntryById(clickedEntryId);
-                            clickedEntry.slideUp();
-                        });
+                        newEntryRow.data('type', 'dynamic');
+                        newEntryRow.appendTo('#searchResults ul');
+                        newEntryRow.find('.english').text(row.english);
+                        newEntryRow.find('.spanish').text(row.spanish);
+                        ;
                     }
-                },
-                errorHandler
-            );
-        }
-    );
-}
-function createEntry() {
-    var date = sessionStorage.currentDate;
-    var calories = $('#calories').val();
-    var food = $('#food').val();
-    db.transaction(
-        function(transaction) {
-            transaction.executeSql(
-                'INSERT INTO entries (date, calories, food) VALUES (?, ?, ?);',
-                [date, calories, food],
-                function(){
-                    refreshEntries();
-                    jQT.goBack();
                 },
                 errorHandler
             );
@@ -104,10 +85,3 @@ function errorHandler(transaction, error) {
     return true;
 }
 
-function deleteEntryById(id) {
-    db.transaction(
-        function (transaction) {
-            transaction.executeSql('DELETE FROM entries WHERE id=?;', [id], null, errorHandler);
-        }
-    );
-}
